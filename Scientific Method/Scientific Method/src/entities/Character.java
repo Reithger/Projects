f3a69f9073601b65063be95fb76e778a938d546f
Mac <39278228+Reithger@users.noreply.github.com>
2018-08-21 19:27:35 -0300

    Started Commenting and Cleaning this up oh god I didn't realize how big this was

--- Scientific Method/Scientific Method/src/entities/Character.java
+++ Scientific Method/Scientific Method/src/entities/Character.java
@@ -1,18 +1,53 @@
 package entities;
+
 import java.util.*;
 
-public class Character extends Player{
+/**
+ * This class models a non-player character, handling their generation while using inherited methods
+ * for much of their behavior. Generation includes the assigning of their identity traits, their
+ * biases, and their name.
+ * 
+ * This class is a part of the entities package.
+ * 
+ * @author Reithger
+ *
+ */
 
+public class Character extends Player{
+	
+//---  Constant Values   ----------------------------------------------------------------------
+
+	/** Constant String[] object containing potential First Names for Characters.*/
 	private String[] NAME_LIST_FIRST = {"Sam", "Jordan", "Aerin", "Jamie", "Morgan", "Taylor", "Billie", "Cameron", "Robin", "Leo", "Fran", "Bailey",
 										"Dakota", "Devin", "Drew", "Ashton", "Jesse", "Kelsey", "Logan", "Mackenzie", "Marley", "Mason", "Quinn",
 										"Reagan", "Rowan", "Ryan", "Shawn", "Ashley", "Casey", "Chris", "Clem", "Connie", "Denny", "Gail", "Izzy"};
+	/** Constant String[] object containing potential Last Names for Characters.*/
 	private String[] NAME_LIST_LAST = {"Jones", "Myers", "Seamus", "Moroaner", "Douglas", "Brown", "Miller", "Stewart", "Sanchez", "Davis", "Perez",
 									   "Martinez", "Thompson", "Robinson", "Clark", "Campbell", "Alexander", "Foster", "Patterson", "Baker", "Kelly",
 									   "Lewis", "Lee", "Walker", "Watson", "Russel", "Bryant", "Carter", "Green", "Hayes", "Diaz", "Taylor", "Garcia"};
 	
+//---  Instance Variables   -------------------------------------------------------------------
+	
+	/** double[][] instance variable describing the biases held by this Character.*/
 	private double[][] bias;
+	/** int value describing their natural x-coordinate location (where they should be on the map.)*/
 	private int boundX;
+	/** int value describing their natural y-coordinate location (where they should be on the map.)*/
 	private int boundY;
+	
+//---  Constructors   -------------------------------------------------------------------------
+	
+	/**
+	 * Constructor for Character object that assigns a default location to the Character and initializes
+	 * their identity and bias matrices; it also takes a default image for them, names them, and assign
+	 * the provided titles and ranking.
+	 * 
+	 * @param x - int value describing the x coordinate location that this Character is set to.
+	 * @param y - int value describing the y coordinate location that this Character is set to.
+	 * @param imageLoc - String object denoting a file path to the default image for this Character.
+	 * @param newRanking - int value describing the ranking in the institution for this Character.
+	 * @param newTitle - String object representing the title assigned to this Character.
+	 */
 	
 	public Character(int x, int y, String imageLoc, int newRanking, String newTitle){
 		super(x, y, imageLoc);
@@ -27,11 +62,30 @@ public class Character extends Player{
 		assignTitle(newTitle);
 	}
 	
+	/**
+	 * Simple Constructor for Character objects; assigns the provided location, name, and title to
+	 * the Character, as well as using the specified file path for their image.
+	 * 
+	 * @param x - int value describing the x coordinate location that this Character is set to.
+	 * @param y - int value describing the y coordinate location that this Character is set to.
+	 * @param name - String object representing the desired name for this Character.
+	 * @param title - String object representing the title assigned to this Character.
+	 * @param imageLoc - String object denoting a file path to the default image for this Character.
+	 */
+	
 	public Character(int x, int y, String name, String title, String imageLoc){
 		super(x, y, imageLoc);
 		setName(name);
 		assignTitle(title);
 	}
+	
+//---  Assignation   --------------------------------------------------------------------------
+	
+	/**
+	 * This method assists in the initialization of a new Character by assigning values to their identity
+	 * matrix and defining their visual appearance. Helper methods handle the assigning of their sex, gender,
+	 * race, sexuality, disability status, and socio-economic status.
+	 */
 	
 	private void fillIdent(){
 		Random rand = new Random();
@@ -48,6 +102,13 @@ public class Character extends Player{
 		for(int i = 0; i < randShoe; i++)
 		  cycleShoe();
 	}
+	
+	/**
+	 * This method assists in the initialization of a new Character by assigning values to their bias matrix as
+	 * affected by their already generated identity and a roll of the dice. Values default to an even 50 and from
+	 * there are shifted. Typically one will be more mindful towards their own identity and less so to others,
+	 * with the additional randomized value being added/subtracted from that.
+	 */
 	
 	private void fillBias(){
 		int[] count = {3, 3, 5, 6, 7, 3, 1};
@@ -71,22 +132,62 @@ public class Character extends Player{
 			}
 		}
 	}
+	
+//---  Getter Methods   -----------------------------------------------------------------------
+	
+	/**
+	 * Getter method that requests the assigned location of this Character's x coordinate.
+	 * 
+	 * @return - Returns an int value describing the x coordinate that has been assigned as this Character's home.
+	 */
 
 	public int getHomeX(){
 		return boundX;
 	}
 	
+	/**
+	 * Getter method that requests the assigned location of this Character's y coordinate.
+	 * 
+	 * @return - Returns an int value describing the y coordinate that has been assigned as this Character's home.
+	 */
+	
 	public int getHomeY(){
 		return boundY;
 	}
+	
+//---  Setter Methods   -----------------------------------------------------------------------
+	
+	/**
+	 * Setter method that assigns a new value as the x coordinate of the Character's home location.
+	 * 
+	 * @param in - int value provided as the new x coordinate of the Character object's home location.
+	 */
 	
 	public void setHomeX(int in){
 		boundX = in;
 	}
 	
+	/**
+	 * Setter method that assigns a new value as the y coordinate of the Character's home location.
+	 * 
+	 * @param in - int value provided as the new y coordinate of the Character object's home location.
+	 */
+	
 	public void setHomeY(int in){
 		boundY = in;
 	}
+	
+//---  Operations   ---------------------------------------------------------------------------
+	
+	/**
+	 * This method handles the calculation of how inclined the Character object is towards the player's
+	 * performance as a derivative of both the player's score and the Character's opinion towards their
+	 * identity, applying their Bias Matrix values to the player's Identity Matrix. 
+	 * 
+	 * @param iden - boolean[][] object representing the identity of the player.
+	 * @param score - double value representing the quality of the player's work.
+	 * @return - Returns a double value as the calculated perceived score given to the player.
+	 */
 	
 	public double processResult(boolean[][] iden, double score){
 		double base = 1.0;
@@ -100,6 +201,16 @@ public class Character extends Player{
 		return base * score * (5 - getRank());
 	}
 	
+//---  Assignation Helper   -------------------------------------------------------------------
+	
+	/**
+	 * This method handles the assigning of a Character object's sex and gender identity values.
+	 * 
+	 * @param rank - int value describing their position in the institute; it affects likelihood of certain outcomes.
+	 * @param rand - Random object provided to save the time of generating a new one very often.
+	 * @return - Returns an int value defining the sex and gender identity values.
+	 */
+	
 	private int randomizeSexGender(int rank, Random rand){
 		int hold = rand.nextInt(100);
 		switch(rank){
@@ -111,6 +222,14 @@ public class Character extends Player{
 			default: return 0;
 		}
 	}
+	
+	/**
+	 * This method handles the assigning of a Character object's race identity value.
+	 * 
+	 * @param rank - int value describing their position in the institute; it affects likelihood of certain outcomes.
+	 * @param rand - Random object provided to save the time of generating a new one very often.
+	 * @return - Returns an int value defining the sex and gender identity values.
+	 */
 	
 	private int randomizeRace(int rank, Random rand){
 		int hold = rand.nextInt(100);
@@ -124,12 +243,28 @@ public class Character extends Player{
 		}
 	}
 	
+	/**
+	 * This method handles the assigning of a Character object's sexuality identity value.
+	 * 
+	 * @param rank - int value describing their position in the institute; it affects likelihood of certain outcomes.
+	 * @param rand - Random object provided to save the time of generating a new one very often.
+	 * @return - Returns an int value defining the sex and gender identity values.
+	 */
+	
 	private int randomizeSexuality(int rank, Random rand){
 		int hold = rand.nextInt(100);
 		switch(rank){
 			default: return multiItemProbability(hold, 80, 88, 92, 94, 96);
 		}
 	}
+	
+	/**
+	 * This method handles the assigning of a Character object's disability identity value.
+	 * 
+	 * @param rank - int value describing their position in the institute; it affects likelihood of certain outcomes.
+	 * @param rand - Random object provided to save the time of generating a new one very often.
+	 * @return - Returns an int value defining the sex and gender identity values.
+	 */
 	
 	private int randomizeDisability(int rank, Random rand){
 		int hold = rand.nextInt(100);
@@ -143,6 +278,14 @@ public class Character extends Player{
 		}
 	}
 	
+	/**
+	 * This method handles the assigning of a Character object's socio-economic identity value.
+	 *
+	 * @param rank - int value describing their position in the institute; it affects likelihood of certain outcomes.
+	 * @param rand - Random object provided to save the time of generating a new one very often.
+	 * @return - Returns an int value defining the sex and gender identity values.
+	 */
+	
 	private int randomizeEconomicSocial(int rank, Random rand){
 		int hold = rand.nextInt(100);
 		switch(rank){
@@ -155,6 +298,17 @@ public class Character extends Player{
 		}
 	}
 	
+	/**
+	 * This method simplifies the process of calculating a result based on varying percentage
+	 * likelihoods; it takes in a calculated random value and a list of values denoting the ranges
+	 * for certain results to occur, and then iterates through that list to see which option was
+	 * selected.
+	 * 
+	 * @param calc - int value denoting the result of a randomization to choose an identity value.
+	 * @param chance - varargs ... array of int values describing the likelihood of different outcomes. 
+	 * @return - Returns an int value describing the result of checking the calc value against the chance values.
+	 */
+	
 	private int multiItemProbability(int calc, int ... chance){
 		for(int i = 0; i < chance.length; i++){
 			if(calc < chance[i]){
@@ -163,4 +317,5 @@ public class Character extends Player{
 		}
 		return chance.length-1;
 	}
+	
 }
--- Scientific Method/Scientific Method/src/entities/Entity.java
+++ Scientific Method/Scientific Method/src/entities/Entity.java
@@ -1,4 +1,5 @@
 package entities;
+
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
@@ -11,19 +12,41 @@ import javax.imageio.ImageIO;
 
 import mechanics.InteractFrame;
 
-public class Entity {
+/**
+ * This class models a graphical object that is displayed by this program, assigning location, size,
+ * and appearance to the object. It is extended by other classes as the fundamental element in displaying
+ * complicated objects.
+ * 
+ * This class is a part of the entities package.
+ * 
+ * @author Reithger
+ *
+ */
 
+public class Entity {
+	
+//---  Instance Variables   -------------------------------------------------------------------
+
+	/** int instance variable describing the x coordinate location of this Entity object*/
 	private int positionX;
+	/** int instance variable describing the y coordinate location of this Entity object*/
 	private int positionY;
+	/** int instance variable describing the width of this Entity object (visually)*/
 	private int width;
+	/** int instance variable describing the height of this Entity object (visually)*/
 	private int height;
+	/** Pixel[][] instance variable object containing the */
 	private Pixel[][] visual;	//If animated, then add third dimension where 0 is the at-rest (though that may have a bit, too)
+	
+//---  Constructors   -------------------------------------------------------------------------
 	
 	public Entity(int x, int y, String visualPath){
 		setX(x);
 		setY(y);
 		setVisual(visualPath);
 	}
+	
+//---  Setter Methods   -----------------------------------------------------------------------
 	
 	public void setX(int x){
 		positionX = x;
@@ -32,40 +55,6 @@ public class Entity {
 	public void setY(int y){
 		positionY = y;
 	}
-	
-	public int getX(){
-		return positionX;
-	}
-	
-	public int getY(){
-		return positionY;
-	}
-	
-	public void moveX(int x){
-		setX(getX() + x);
-	}
-	
-	public void moveY(int y){
-		setY(getY() + y);
-	}
-	
-	public int getWidth(){
-		return width;
-	}
-	
-	public int getHeight(){
-		return height;
-	}
-	
-	public Image retrieveImage(String path) {
-		try {
-			return ImageIO.read(InteractFrame.class.getResource(path));
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-}
 
 	public void setVisual(String fileLocation){
 		//Generic is to just grab and paste the image from the Assets file, overload for templates and filling.
@@ -88,13 +77,43 @@ public class Entity {
 		height = visual[0].length;
 	}
 	
+//---  Getter Methods   -----------------------------------------------------------------------
+	
+	public int getX(){
+		return positionX;
+	}
+	
+	public int getY(){
+		return positionY;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+
 	public Pixel[][] getVisual(){
 		return visual;
+	}
+
+//---  Manipulations   ------------------------------------------------------------------------
+	
+	public void moveX(int x){
+		setX(getX() + x);
+	}
+	
+	public void moveY(int y){
+		setY(getY() + y);
 	}
 
 	public void adjustVisual(int x, int y, Color col){
 		visual[x][y] = new Pixel(col);
 	}
+
+//---  Drawing   ------------------------------------------------------------------------------
 	
 	public void drawToScreen(Graphics g){
 		for(int i = 0; i < visual[0].length; i++){
@@ -156,4 +175,17 @@ public class Entity {
 			}
 		}
 	}
+
+//---  Mechanics   ----------------------------------------------------------------------------
+	
+	public Image retrieveImage(String path) {
+		try {
+			return ImageIO.read(InteractFrame.class.getResource(path));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 }
